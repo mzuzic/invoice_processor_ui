8a7fb54142e86bb77c8295f3395f113524a44bf9
Marko Zuzic <markozuzic@nordnet.rs>
2025-09-20 19:43:26 +0200

    Fix typo

--- src/components/upload/ProcessingView.tsx
+++ src/components/upload/ProcessingView.tsx
@@ -13,7 +13,7 @@ export function ProcessingView({ documents, downloadButton }: ProcessingViewProp
         <thead className="bg-gray-50">
           <tr>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Filename
+              File name
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               Document Type
